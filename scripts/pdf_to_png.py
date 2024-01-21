dee5fb8121bdcbf2c0fddbc58fa6c48b2e09c940
AnixDrone <tomislavignjatov@rocketmail.com>
2024-01-22 00:11:39 +0100

    minor refactor

--- scripts/pdf_to_png.py
+++ scripts/pdf_to_png.py
@@ -15,8 +15,7 @@ def pdf2png(root_path, pdf_file_name):
     print(pdf_path)
     pages = pdf2image.convert_from_path(pdf_path)
     for idx, page in tqdm.tqdm(enumerate(pages), total=len(pages)):
-        png_path = os.path.join(
-            pdf_path[:-4], f"{pdf_file_name[:-4]}_{idx}.png")
+        png_path = os.path.join(pdf_path[:-4], f"{pdf_file_name[:-4]}_{idx}.png")
         page.save(png_path, "PNG")
 
 
@@ -24,6 +23,6 @@ if __name__ == "__main__":
     market = "/home/tomislav/Projects/Angebot/LETOCI/VERO"
     something = useless_function()
     print(something)
-    print('example')
+    print("example")
     # for file in os.listdir(market):
     #    pdf2png(market, file)
